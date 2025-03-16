2c1f303d9373b167ee9ee77894b87c9dacd1e1ae
rh0 <125304115+rehite1@users.noreply.github.com>
2025-03-16 21:27:46 +0530

    added precision and readme

--- bisect/main.c
+++ bisect/main.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include <math.h>
 
-double u,v,b;
+double u=0,v=0,b=0,bp=0;
 #include "problem.h"
 #ifndef start
 	#define start 0
@@ -11,18 +11,24 @@ double u,v,b;
 	#define stop 1
 #endif
 #ifndef iter
-	#define iter 20
+	#define iter 50
+#endif
+#ifndef precision
+	#define precision 0
 #endif
 int main(){
 	u=start;v=stop;
 	for(int i=0;i<iter;i++){
-		printf("iteration:%d\na:\t%lf\nb:\t%lf\nm:\t%lf\n",i,u,v,b);
+		printf("iteration:%d\na:\t%lf\tb:\t%lf\n\tm:\t%lf\n",i,u,v,b);
 		if(f(u)*f(v)>0)
 			assert(0);
 		b=(u+v)/2.0;
 		if(f(b)==0) break;
 		else if(f(u)*f(b)<0) v=b;
 		else if(f(b)*f(v)<0) u=b;
+
+		if(bp-b<=precision && bp-b>=-1.0*precision) break;
+		bp=b;
 
 	}
 	printf("solution=%lf\n",b);
--- bisect/problem.h
+++ bisect/problem.h
@@ -1,3 +1,4 @@
 //#define start 0
 //#define stop 1
+#define precision 0.000001
 #define f(x) (pow(x,3)-5.0*x+1)
