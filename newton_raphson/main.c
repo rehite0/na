0a5e5cadbaf1931a8add3c607365a676e798ba72
rh0 <125304115+rehite1@users.noreply.github.com>
2025-03-18 23:33:49 +0530

    newton_raphson method added

---
+++ newton_raphson/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+double xn,xm;
+#include "problem.h"
+#ifndef x
+	#define xo 0
+#endif
+#ifndef iter
+	#define iter 10
+#endif
+#ifndef precision
+	#define precision 0
+#endif
+#ifndef f
+	#error
+#endif
+#ifndef df
+	#error
+#endif
+
+
+int main(){
+	xm=0;xn=xo;
+	for(int i=0;i<iter;i++){
+		printf("iteration:%d\nx:%.10lf\tf(x):%lf\n",i,xn,f(xn));
+		xm=xn-(f(xn)/df(xn));
+		if((xm-xn<=precision && xm-xn>=-1.0*precision)||f(xm)==0) break;
+		xn=xm;
+
+	}
+	printf("solution=%.10lf\n",xm);
+}
---
+++ newton_raphson/problem.h
@@ -0,0 +1,5 @@
+//#define xo 0
+//#defin iter 50
+#define precision 0.00000001
+#define f(x) (pow(x,3)-5.0*x+1)
+#define df(x) (3.0*pow(x,2)-5.0)
